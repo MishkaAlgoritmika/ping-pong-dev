ebef2c6be645aa7e7b9c43bd90fcb08eb297576e
MishkaAlgoritmika <mishkapetran@gmail.com>
2024-04-27 09:08:56 +0400

    Уменьшение размера ракеток при касании с мячом

--- ping-pong.py
+++ ping-pong.py
@@ -1,32 +1,51 @@
 from pygame import *
+
 '''Необходимые классы'''
 #класс-родитель для спрайтов
 class GameSprite(sprite.Sprite):
-   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
-       super().__init__()
-       self.image = transform.scale(image.load(player_image), (wight, height)) #вместе 55,55 - параметры
-       self.speed = player_speed
-       self.rect = self.image.get_rect()
-       self.rect.x = player_x
-       self.rect.y = player_y
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (width, height)) #вместе 55,55 - параметры
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
 
-   def reset(self):
-       window.blit(self.image, (self.rect.x, self.rect.y))
+        #для уменьшения ракеток
+        self.width = width
+        self.height = height
+        self.player_image = player_image
+    
+    def set_default(self, height):
+        self.height = height
+        self.image = transform.scale(image.load(self.player_image), (self.width, height))
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-   def update_r(self):
-       keys = key.get_pressed()
-       if keys[K_UP] and self.rect.y > 5:
-           self.rect.y -= self.speed
-       if keys[K_DOWN] and self.rect.y < win_height - 80:
-           self.rect.y += self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        
+        #фикс ухода за нижнюю границу
+        if keys[K_DOWN] and self.rect.y < win_height - self.height:
+            self.rect.y += self.speed
 
-   def update_l(self):
-       keys = key.get_pressed()
-       if keys[K_w] and self.rect.y > 5:
-           self.rect.y -= self.speed
-       if keys[K_s] and self.rect.y < win_height - 80:
-           self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        
+        #фикс ухода за нижнюю границу
+        if keys[K_s] and self.rect.y < win_height - self.height:
+            self.rect.y += self.speed
+    
+    #для уменьшения ракеток
+    def change_size(self):
+        self.height *= 0.9
+        self.image = transform.scale(image.load(self.player_image), (self.width, int(self.height)))
 
 #игровая сцена:
 back = (200, 255, 255) #цвет фона (background)
@@ -86,25 +105,35 @@ while game:
         #При касании мячом ракетки менять направление и скорость мяча
         if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
             speed_x *= -1.2
+            win.fill((255, 255, 255))
+            #будем уменьшать размеры ракеток
+            if sprite.collide_rect(racket1, ball):
+                racket1.change_size()
+            else:
+                racket2.change_size()
       
         #если мяч достигает границ экрана, меняем направление его движения
         if ball.rect.y > win_height - ball_height or ball.rect.y < 0:
             speed_y *= -1
 
-        #если мяч улетел дальше ракетки, выводим условие проигрыша для первого игрока
+        #если мяч улетел дальше ракетки, выводим условие проигрыша для первого игрока, также возвращаем размер ракеток
         if ball.rect.x < 0:
             player2_score += 1
             ball.rect.x =  win_width / 2 - ball_width / 2
             ball.rect.y =  win_height / 2 - ball_height / 2
             speed_x = 3
             speed_x *= -1
+            racket1.set_default(racket_height)
+            racket2.set_default(racket_height)
 
-        #если мяч улетел дальше ракетки, выводим условие проигрыша для второго игрока
+        #если мяч улетел дальше ракетки, выводим условие проигрыша для второго игрока, также возвращаем размер ракеток
         if ball.rect.x > win_width:
             player1_score += 1
             ball.rect.x =  win_width / 2 - ball_width / 2
             ball.rect.y =  win_height / 2 - ball_height / 2
             speed_x *= -1
+            racket1.set_default(racket_height)
+            racket2.set_default(racket_height)
 
 
         score_text = score_font.render(str(player1_score) + " : " + str(player2_score), 1, score_text_color)
@@ -124,7 +153,6 @@ while game:
         racket1.reset()
         racket2.reset()
         ball.reset()
-
 
     display.update()
     clock.tick(FPS)
